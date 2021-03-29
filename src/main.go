de0a1813ee7d5be47f1406703c6ead18922c2912
jinsuSang <twtz0309@naver.com>
2021-03-29 20:50:57 +0900

    feat: add env and tls

--- src/main.go
+++ src/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/shopping-mall-api/src/rest"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	const address string = ":8080"
 	log.Println("Server is running on " + address)
-	err := rest.RunAPI(address)
-	log.Fatal(err)
+	err = rest.RunAPI(address)
+	log.Fatal(err.Error())
 }
